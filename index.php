71e3f5248aae2d48640eb48e7ad97a0df591e8c5
Abhijeet <abhijeetmundhe55@gmail.com>
2022-07-21 03:43:36 +0530

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include './header.php'
 ?>
   
-  <img  height="1000px" width="1000px" src="https://pngimg.com/uploads/bank/bank_PNG4.png" alt="" class="img-fluid">
+  <img  height="500px" width="600px" style="float: right; margin-right: 0%;"src="https://pngimg.com/uploads/bank/bank_PNG4.png" alt="" class="img-fluid">
   <h1>Spark Banking System</h1>
 
   <p>Made by Abhijeet</p>
