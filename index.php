38d522dc4bb5fda6a72aeb3fd686f7ebab53b008
Abhijeet <abhijeetmundhe55@gmail.com>
2022-07-21 15:30:15 +0530

    index update

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 include './header.php'
 ?>
   
-  <img  height="500px" width="600px" style="float: right; margin-right: 0%;"src="https://pngimg.com/uploads/bank/bank_PNG4.png" alt="" class="img-fluid">
+  
+  <img src="https://pngimg.com/uploads/bank/bank_PNG4.png" alt="" class="img-fluid rounded float-end" >
+  <!-- <img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600"> -->
   <h1>Spark Banking System</h1>
+  <div style=padding-top:200px;>
+  <h4>Get access of your account in your home</h4>
 
-  <p>Made by Abhijeet</p>
+  <p>No more waiting in the long queues 
+    ,transfer the money very conveniently<br> with the help of our Sparks Bank System ,
+     a digital wallet platform and online payment system<br> developed  to power in-app, online, and in-person contactless  
+     transactions with mobile phones.</p>
+  </div>
+  <div id="bottom">
+  <p>Made By Abhijeet</P>
+  </div>
+
+  
 
 </body>
 
